61b6bafe4d18f2fe6f539c1a5807f4cbbaaa5e1a
TeBe Margono <tebemargono@gmail.com>
2023-11-22 23:19:22 +0700

    Belajar perintah if, elif, dan else di Python

--- tes.py
+++ tes.py
@@ -1,1 +1,10 @@
-print("Hello there!")
+input_hari = input("Masukkan nama hari ").lower()
+
+if input_hari == "senin":
+    print ("Sekarang hari Senin")
+elif input_hari == 'selasa':
+    print("Sekarang hari Selasa")
+else:
+    print("Lanjut!")
+
+print ("Ini selalu diprint")
