e8fbad58707c72c54887afe3874c3ef2ee074571
TeBe Margono <tebemargono@gmail.com>
2023-11-23 11:33:16 +0700

    Loops in Python

---
+++ 2.18 Loops in Python.py
@@ -0,0 +1,28 @@
+# number = 7
+
+# while True:
+    
+#     user_input = input("Would you like to play? (Y/n) ").lower()
+    
+#     if user_input == 'n':
+#         break
+    
+#     user_number = int(input("Guess our number: "))
+#     if user_number == number:
+#         print("You guessed correctly!")
+#     elif abs(number - user_number) == 1:
+#         print("You were off by one")
+#     else:
+#         print("Sorry, it's wrong!")
+
+# friends = ['Agus','Badu','Cici','Dodo','Eko']
+
+# for friend in friends:
+#     print(f"{friend} is my friend")
+
+grades = [35,67,98, 100,100]
+
+total = sum(grades)
+amount=len(grades)
+
+print(total/amount)
