088a3788a679a5a11bf6f291cc1440149218fb4f
TeBe Margono <tebemargono@gmail.com>
2023-11-24 10:14:43 +0700

    Perubahan kedua

---
+++ 2.19 List comprehentions in Python.py
@@ -0,0 +1,8 @@
+friends = ['ajo','badu','charli','dedi','dodo','edi']
+starts_s = [friend for friend in friends if friend.startswith('e')]
+
+# for friend in friends:
+#     if friend.startswith('d'):
+#         starts_s.append(friend)
+
+print(starts_s)
